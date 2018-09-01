2ed9915d3b0afa7f519072dd24a6ed089527ecdd
MrLYC <imyikong@gmail.com>
2018-09-01 22:08:56 +0800

    parse flag when default plugin start

--- plugins/commands/default.go
+++ plugins/commands/default.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"flag"
+
 	"github.com/MrLYC/gokit/config"
 	"github.com/MrLYC/gokit/plugins"
 	"github.com/google/subcommands"
@@ -17,9 +19,12 @@ func (p *DefaultPlugin) Start(conf config.Configuration) error {
 	if err != nil {
 		return err
 	}
+
 	subcommands.Register(subcommands.HelpCommand(), config.Attrs.GetString("plugins.commands.group.help"))
 	subcommands.Register(subcommands.FlagsCommand(), config.Attrs.GetString("plugins.commands.group.flags"))
 	subcommands.Register(subcommands.CommandsCommand(), config.Attrs.GetString("plugins.commands.group.commands"))
+
+	flag.Parse()
 	return nil
 }
 
